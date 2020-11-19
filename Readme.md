6059306a30ac8b61f413d30abc884d1a8dafed05
Eugenia.Simonova <eugenia.simonova@devexpress.com>
2020-11-19 14:31:33 +0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,22 @@
+<!-- default file list -->
+*Files to look at*:
+
+* [Window1.xaml](./CS/ScatterPolarLine/Form1.cs) (VB: [Window1.xaml](./VB/ScatterPolarLine/Form1.cs))
+<!-- default file list end -->
+# How to bind a Scatter Polar Line Chart to a Data Source
+
+ The following example binds a [Scatter Polar Line Chart](xref:113900) to a data table:
+
+* Create an instance of the [Chart Control](xref:DevExpress.XtraCharts.ChartControl) class and specify the [ChartControl.DataSource](xref:DevExpress.XtraCharts.ChartControl.DataSource) property.
+
+* Create a [Series](xref:DevExpress.XtraCharts.Series) and specify the [Series.Name](xref:DevExpress.XtraCharts.Series.Name) and [ViewType](xref:DevExpress.XtraCharts.ViewType) in the series constructor. Set the [Series.ArgumentDataMember](xref:DevExpress.XtraCharts.SeriesBase.ArgumentDataMember) property to a data source field that contains series point arguments, and the [Series.ValueDataMembers](xref:DevExpress.XtraCharts.SeriesBase.ValueDataMembers) property - to a data source field that contains values. Add this series to the [ChartControl.Series](xref:DevExpress.XtraCharts.ChartControl.Series) collection.
+ 
+* Cast the [Series.View](xref:DevExpress.XtraCharts.SeriesBase.View) property to the [ScatterPolarLineSeriesView](xref:DevExpress.XtraCharts.ScatterPolarLineSeriesView) and use the [RadarLineSeriesView.MarkerVisibility](xref:DevExpress.XtraCharts.RadarLineSeriesView.MarkerVisibility) property to hide markers. Set the [ScatterPolarLineSeriesView.Closed](xref:DevExpress.XtraCharts.RadarLineSeriesView.Closed) property to false to hide the line that connects the first and last point. See the following section for more information about the chart customization: [Change Chart Appearance](#change-chart-appearance).    
+
+* Use the [Legend.Visibility](xref:DevExpress.XtraCharts.Legend.Visibility) property to hide the chart's legend.
+
+* Call the [Titles.Add](xref:DevExpress.XtraCharts.ChartTitleCollection.Add(DevExpress.XtraCharts.ChartTitle)) method to add a chart's title to the [ChartControl.Titles](xref:DevExpress.XtraCharts.ChartControl.Titles) collection.
+
+<br/>
+
+
---
+++ VB/ScatterPolarLine/Form1.vb
@@ -0,0 +1,63 @@
+﻿Imports DevExpress.XtraCharts
+Imports System
+Imports System.Windows.Forms
+Imports DevExpress.Utils
+Imports System.Data
+
+Namespace ScatterPolarLine
+	Partial Public Class Form1
+		Inherits Form
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+			' Create a new chart.
+			Dim scatterPolarLineChart As New ChartControl()
+
+			' Add the chart to the form.
+			scatterPolarLineChart.Dock = DockStyle.Fill
+			Me.Controls.Add(scatterPolarLineChart)
+			scatterPolarLineChart.DataSource = CreateChartData()
+
+			' Create a scatter polar line series.
+			Dim series As New Series("Series 1", ViewType.ScatterPolarLine)
+			series.ArgumentDataMember = "Angle"
+			series.ValueDataMembers.AddRange(New String() { "Distance" })
+			scatterPolarLineChart.Series.Add(series)
+			Dim view As ScatterPolarLineSeriesView = CType(series.View, ScatterPolarLineSeriesView)
+
+			' Hide markers.
+			view.MarkerVisibility = DefaultBoolean.False
+			' Hide the line that connects the first and last point.
+			view.Closed = False
+
+			' Hide the legend (if necessary).
+			scatterPolarLineChart.Legend.Visibility = DefaultBoolean.False
+
+			' Add a title to the chart (if necessary).
+			scatterPolarLineChart.Titles.Add(New ChartTitle())
+			scatterPolarLineChart.Titles(0).Text = "Archimedean Spiral"
+		End Sub
+
+		Private Function CreateChartData() As DataTable
+			Dim table As New DataTable("Table1")
+			table.Columns.Add("Angle", GetType(Double))
+			table.Columns.Add("Distance", GetType(Double))
+
+			Dim minAngle As Double = 0
+			Dim maxAngle As Double = 720
+			Dim intervalsCount As Integer = 72
+			Dim angleStep As Double = (maxAngle - minAngle) / CDbl(intervalsCount)
+			For pointIndex As Integer = 0 To intervalsCount
+				Dim angle As Double = minAngle + pointIndex * angleStep
+				Dim angleRadians As Double = angle * Math.PI / 180.0
+				Dim distance As Double = angleRadians
+				Dim normalizeAngle As Double = angle Mod 360
+				table.Rows.Add(New Object() { normalizeAngle, distance })
+			Next pointIndex
+			Return table
+		End Function
+	End Class
+End Namespace
